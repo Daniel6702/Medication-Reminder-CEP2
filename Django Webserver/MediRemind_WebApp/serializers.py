74d30b7d3e78112b4a0df91d694d9de134d7f802
Daniel Mærsk Pedersen <pedersendaniel3561@gmail.com>
2024-03-15 10:04:06 +0100

    Add .gitattributes file

--- Django Webserver/MediRemind_WebApp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import HeucodEvent
-
-class HeucodEventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HeucodEvent
-        fields = '__all__'
